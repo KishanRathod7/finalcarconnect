eebf4e1832585ab04dabbd387ecacccca8ad1540
devang <devmuraridevang361@gmail.com>
2023-08-28 09:37:35 +0530

    initial commit

--- app/src/main/java/com/example/carconnect/MainActivity.kt
+++ app/src/main/java/com/example/carconnect/MainActivity.kt
@@ -1,14 +1,20 @@
-package com.example.carconnect;
+package com.example.carconnect
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    var mercedescard: CardView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mercedescard = findViewById<View>(R.id.mercedescard) as CardView
+        mercedescard!!.setOnClickListener {
+            val intent = Intent(this@MainActivity, mercedesrecyscleview::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/carconnect/bmwrecycleview.kt
@@ -0,0 +1,11 @@
+package com.example.carconnect
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class bmwrecycleview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmwrecycleview)
+    }
+}
